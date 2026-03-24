d4b7c5f1e13f496f8d1a0c71965d756d1f3a8477
River Nguyen <ngiang1309@gmail.com>
2026-03-24 14:55:36 +0700

    Refactor fetchData function to include fetch options for revalidation and update import paths for environment configurations in fetch modules.

---
+++ src/lib/swr.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import ENV from '@/configs/env'
+
+const basePath = ENV.API!
+export const fetcher = async (url: string, baseCustom?: string) => {
+  const base = baseCustom || `${ENV.CMS}${basePath}`
+  const response = await fetch(`${base}${url}`)
+  if (!response.ok) {
+    throw new Error('An error occurred while fetching the data.')
+  }
+  return response.json()
+}
+
+export const fetcherCMS = async (url: string) => {
+  const response = await fetch(`${ENV.CMS}${url}`)
+  if (!response.ok) {
+    throw new Error('An error occurred while fetching the data.')
+  }
+  return response.json()
+}
